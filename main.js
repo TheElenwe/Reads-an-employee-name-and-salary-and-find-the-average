62238aaaaeb9f7e7aac8d71e4e203ebc15f74d3f
unknown <you@example.com>
2021-01-31 22:55:32 +0200

    First commi

---
+++ main.js
@@ -0,0 +1,25 @@
+let readlineSync = require("readline-sync");
+sum = 0;
+for (employee = 0; employee < 10; employee++) {
+  let employee = readlineSync.question("Input employee name: ");
+  if (employee === "quit") {
+    break;
+  }
+  salary = Number(readlineSync.question("Input employee salary: "));
+  sum += salary;
+}
+avg = sum / 4;
+console.log(` the total amount of salaries is ${avg}$`);
+while (true) {
+  salary = String(readlineSync.question("give numbers"));
+  if (salary === "quit") {
+    break;
+  }
+  if (salary > avg) {
+    console.log("salary is above average ");
+    employee--;
+  } else {
+    console.log("salary is bellow average ");
+  }
+}
+console.log(employee);
