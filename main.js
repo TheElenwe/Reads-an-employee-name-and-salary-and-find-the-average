c90faa3b7d6c331d5412be73bf32821d7484f41c
TheElenwe <73945429+TheElenwe@users.noreply.github.com>
2021-01-31 23:44:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-let readlineSync = require("readline-sync");
-sum = 0;
-for (employee = 0; employee < 10; employee++) {
-  let employee = readlineSync.question("Input employee name: ");
-  if (employee === "quit") {
-    break;
-  }
+const readlineSync = require("readline-sync");
+let sum = 0;
+for (employee = 0; employee <4; employee++) {
+  const employee = readlineSync.question("Input employee name: ");
   salary = Number(readlineSync.question("Input employee salary: "));
   sum += salary;
 }
 avg = sum / 4;
 console.log(` the total amount of salaries is ${avg}$`);
-while (true) {
-  salary = String(readlineSync.question("give numbers "));
-  if (salary === "quit") {
+while(true) {
+  salary = String(readlineSync.question("Input employee salary Again!!...if is above or below average salary "));
+  if(salary === "quit"){
     break;
   }
   if (salary > avg) {
